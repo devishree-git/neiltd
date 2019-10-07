6491d9f1c18480cd1c12c319b1761e4ef44e0770
hyperthinkkenya <45934044+hyperthinkkenya@users.noreply.github.com>
2019-10-07 14:31:56 +0300

    Add files via upload

---
+++ product_image_upload/__manifest__.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+# Part of AppJetty. See LICENSE file for full copyright and licensing details.
+
+{
+    'name': 'CSV Product Images Import',
+    'author': 'AppJetty',
+    'license': 'OPL-1',
+    'version': '12.0.1.0.0',
+    'category': 'Sales',
+    'website': 'https://www.appjetty.com',
+    'description': """
+CSV Product Images Import
+Mass Image Upload
+Import Product Images From URL
+Import Product Images
+Import product images from URL Using CSV
+Import Multiple Product Images From URL
+Upload Multiple Images
+Import product Images From URL through CSV
+Set Product Image From URL
+upload images
+	""",
+    'depends': ['base_import', 'website_sale'],
+    'images': ['static/description/splash-screen.png'],
+    'price': 35.00,
+    'currency': 'EUR',
+    'installable': True,
+    'auto_install': False,
+    'support': 'support@appjetty.com',
+}
